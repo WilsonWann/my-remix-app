7154415c620ef5a467594452d21004e8a5dadf0f
musense <musense.ad@gmail.com>
2023-12-18 17:44:59 +0800

    initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -1,49 +1,70 @@
+import type { LinksFunction } from '@remix-run/node'
+
 import {
   Form,
+  Link,
   Links,
   LiveReload,
   Meta,
+  Outlet,
   Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+  ScrollRestoration
+} from '@remix-run/react'
+
+import appStylesHref from './app.css'
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: appStylesHref }]
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1'
+        />
         <Meta />
         <Links />
       </head>
       <body>
-        <div id="sidebar">
+        <div id='sidebar'>
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form
+              id='search-form'
+              role='search'
+            >
               <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
+                id='q'
+                aria-label='Search contacts'
+                placeholder='Search'
+                type='search'
+                name='q'
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div
+                id='search-spinner'
+                aria-hidden
+                hidden={true}
+              />
             </Form>
-            <Form method="post">
-              <button type="submit">New</button>
+            <Form method='post'>
+              <button type='submit'>New</button>
             </Form>
           </div>
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
             </ul>
           </nav>
+        </div>
+        <div id='detail'>
+          <Outlet />
         </div>
 
         <ScrollRestoration />
@@ -51,5 +72,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
