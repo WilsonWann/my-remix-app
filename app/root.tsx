038c11d14bf89896816866eee86540533b621a4a
musense <musense.ad@gmail.com>
2023-12-19 11:04:06 +0800

    chakra ui theme

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useMemo } from 'react'
 import { withEmotionCache } from '@emotion/react'
-import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme, ChakraProvider, cookieStorageManagerSSR } from '@chakra-ui/react'
 import { ServerStyleContext, ClientStyleContext } from './context'
 
 import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node'
@@ -33,7 +33,8 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url)
   const q = url.searchParams.get('q')
   const contacts = await getContacts(q)
-  return json({ contacts, q })
+  const cookies = request.headers.get('cookie') ?? ''
+  return json({ contacts, q, cookies })
 }
 
 interface DocumentProps {
@@ -44,7 +45,33 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache) =>
   const serverStyleData = useContext(ServerStyleContext)
   const clientStyleData = useContext(ClientStyleContext)
 
+  function getColorMode(cookies: string) {
+    const match = cookies.match(new RegExp(`(^| )${CHAKRA_COOKIE_COLOR_KEY}=([^;]+)`))
+    return match === null ? void 0 : match[2]
+  }
+
+  const DEFAULT_COLOR_MODE: 'dark' | 'light' | null = 'dark'
+
+  const CHAKRA_COOKIE_COLOR_KEY = 'chakra-ui-color-mode'
+
   const { contacts, q } = useLoaderData<typeof loader>()
+  let { cookies } = useLoaderData<typeof loader>()
+
+  if (typeof document !== 'undefined') {
+    cookies = document.cookie
+  }
+
+  let colorMode = useMemo(() => {
+    let color = getColorMode(cookies)
+
+    if (!color && DEFAULT_COLOR_MODE) {
+      cookies += ` ${CHAKRA_COOKIE_COLOR_KEY}=${DEFAULT_COLOR_MODE}`
+      color = DEFAULT_COLOR_MODE
+    }
+
+    return color
+  }, [cookies])
+
   const navigation = useNavigation()
   const submit = useSubmit()
   const searching = navigation.location && new URLSearchParams(navigation.location.search).has('q')
@@ -88,76 +115,84 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache) =>
           />
         ))}
       </head>
-      <body>
-        <div id='sidebar'>
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form
-              id='search-form'
-              onChange={(event) => {
-                const isFirstSearch = q === null
-                submit(event.currentTarget, {
-                  replace: !isFirstSearch
-                })
-              }}
-              role='search'
-            >
-              <input
-                id='q'
-                defaultValue={q || ''}
-                className={searching ? 'loading' : ''}
-                aria-label='Search contacts'
-                placeholder='Search'
-                type='search'
-                name='q'
-              />
-              <div
-                id='search-spinner'
-                aria-hidden
-                hidden={!searching}
-              />
-            </Form>
-            <Form method='post'>
-              <button type='submit'>New</button>
-            </Form>
-          </div>
-          <nav>
-            {contacts.length ? (
-              <ul>
-                {contacts.map((contact) => (
-                  <li key={contact.id}>
-                    <NavLink
-                      className={({ isActive, isPending }) =>
-                        isActive ? 'active' : isPending ? 'pending' : ''
-                      }
-                      to={`contacts/${contact.id}`}
-                    >
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{' '}
-                      {contact.favorite ? <span>★</span> : null}
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>
-                <i>No Contacts</i>
-              </p>
-            )}
-          </nav>
-        </div>
-        <div
-          className={navigation.state === 'loading' && !searching ? 'loading' : ''}
-          id='detail'
+      <body
+        {...(colorMode && {
+          className: `chakra-ui-${colorMode}`
+        })}
+      >
+        <ChakraProvider
+          colorModeManager={cookieStorageManagerSSR(cookies)}
+          theme={theme}
         >
-          {children}
-        </div>
-
+          <div id='sidebar'>
+            <h1>Remix Contacts</h1>
+            <div>
+              <Form
+                id='search-form'
+                onChange={(event) => {
+                  const isFirstSearch = q === null
+                  submit(event.currentTarget, {
+                    replace: !isFirstSearch
+                  })
+                }}
+                role='search'
+              >
+                <input
+                  id='q'
+                  defaultValue={q || ''}
+                  className={searching ? 'loading' : ''}
+                  aria-label='Search contacts'
+                  placeholder='Search'
+                  type='search'
+                  name='q'
+                />
+                <div
+                  id='search-spinner'
+                  aria-hidden
+                  hidden={!searching}
+                />
+              </Form>
+              <Form method='post'>
+                <button type='submit'>New</button>
+              </Form>
+            </div>
+            <nav>
+              {contacts.length ? (
+                <ul>
+                  {contacts.map((contact) => (
+                    <li key={contact.id}>
+                      <NavLink
+                        className={({ isActive, isPending }) =>
+                          isActive ? 'active' : isPending ? 'pending' : ''
+                        }
+                        to={`contacts/${contact.id}`}
+                      >
+                        {contact.first || contact.last ? (
+                          <>
+                            {contact.first} {contact.last}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                        )}{' '}
+                        {contact.favorite ? <span>★</span> : null}
+                      </NavLink>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>
+                  <i>No Contacts</i>
+                </p>
+              )}
+            </nav>
+          </div>
+          <div
+            className={navigation.state === 'loading' && !searching ? 'loading' : ''}
+            id='detail'
+          >
+            {children}
+          </div>
+        </ChakraProvider>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
@@ -166,10 +201,20 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache) =>
   )
 })
 
+const colors = {
+  brand: {
+    900: '#1a36d5',
+    800: '#153e75',
+    700: '#2a69ac'
+  }
+}
+
+const theme = extendTheme({ colors })
+
 export default function App() {
   return (
     <Document>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Outlet />
       </ChakraProvider>
     </Document>
