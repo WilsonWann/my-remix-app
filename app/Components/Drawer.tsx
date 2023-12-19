2ee52deec4eabb422154bf0c03a7b88ed4acae28
musense <musense.ad@gmail.com>
2023-12-19 12:02:31 +0800

    將sidebar模組分離出來

---
+++ app/Components/Drawer.tsx
@@ -0,0 +1,99 @@
+import { Form, NavLink, useNavigation, useSubmit } from '@remix-run/react'
+import React, { useEffect } from 'react'
+import { ContactRecord } from '../data'
+
+type Props = {
+  children: React.ReactNode
+  contacts: ContactRecord[]
+  q: string | null
+}
+
+const Drawer = (props: Props) => {
+  const { children, contacts, q } = props
+
+  const navigation = useNavigation()
+  const submit = useSubmit()
+  const searching = navigation.location && new URLSearchParams(navigation.location.search).has('q')
+
+  useEffect(() => {
+    const searchField = document.getElementById('q')
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || ''
+    }
+  }, [q])
+
+  return (
+    <>
+      <div id='sidebar'>
+        <h1>Remix Contacts</h1>
+        <div>
+          <Form
+            id='search-form'
+            onChange={(event) => {
+              const isFirstSearch = q === null
+              submit(event.currentTarget, {
+                replace: !isFirstSearch
+              })
+            }}
+            role='search'
+          >
+            <input
+              id='q'
+              defaultValue={q || ''}
+              className={searching ? 'loading' : ''}
+              aria-label='Search contacts'
+              placeholder='Search'
+              type='search'
+              name='q'
+            />
+            <div
+              id='search-spinner'
+              aria-hidden
+              hidden={!searching}
+            />
+          </Form>
+          <Form method='post'>
+            <button type='submit'>New</button>
+          </Form>
+        </div>
+        <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink
+                    className={({ isActive, isPending }) =>
+                      isActive ? 'active' : isPending ? 'pending' : ''
+                    }
+                    to={`contacts/${contact.id}`}
+                  >
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{' '}
+                    {contact.favorite ? <span>★</span> : null}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No Contacts</i>
+            </p>
+          )}
+        </nav>
+      </div>
+      <div
+        className={navigation.state === 'loading' && !searching ? 'loading' : ''}
+        id='detail'
+      >
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default Drawer
--- app/root.tsx
+++ app/root.tsx
@@ -6,20 +6,17 @@ import { ServerStyleContext, ClientStyleContext } from './context'
 import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node'
 import { json, redirect } from '@remix-run/node'
 import {
-  Form,
-  NavLink,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
-  useNavigation,
-  useSubmit
+  useLoaderData
 } from '@remix-run/react'
 
 import appStylesHref from './app.css'
+import Drawer from './Components/Drawer'
 import { createEmptyContact, getContacts } from './data'
 
 export let links: LinksFunction = () => [{ rel: 'stylesheet', href: appStylesHref }]
@@ -50,7 +47,7 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache) =>
     return match === null ? void 0 : match[2]
   }
 
-  const DEFAULT_COLOR_MODE: 'dark' | 'light' | null = 'dark'
+  const DEFAULT_COLOR_MODE: 'dark' | 'light' | null = 'light'
 
   const CHAKRA_COOKIE_COLOR_KEY = 'chakra-ui-color-mode'
 
@@ -71,17 +68,6 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache) =>
 
     return color
   }, [cookies])
-
-  const navigation = useNavigation()
-  const submit = useSubmit()
-  const searching = navigation.location && new URLSearchParams(navigation.location.search).has('q')
-
-  useEffect(() => {
-    const searchField = document.getElementById('q')
-    if (searchField instanceof HTMLInputElement) {
-      searchField.value = q || ''
-    }
-  }, [q])
 
   // Only executed on client
   useEffect(() => {
@@ -124,74 +110,12 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache) =>
           colorModeManager={cookieStorageManagerSSR(cookies)}
           theme={theme}
         >
-          <div id='sidebar'>
-            <h1>Remix Contacts</h1>
-            <div>
-              <Form
-                id='search-form'
-                onChange={(event) => {
-                  const isFirstSearch = q === null
-                  submit(event.currentTarget, {
-                    replace: !isFirstSearch
-                  })
-                }}
-                role='search'
-              >
-                <input
-                  id='q'
-                  defaultValue={q || ''}
-                  className={searching ? 'loading' : ''}
-                  aria-label='Search contacts'
-                  placeholder='Search'
-                  type='search'
-                  name='q'
-                />
-                <div
-                  id='search-spinner'
-                  aria-hidden
-                  hidden={!searching}
-                />
-              </Form>
-              <Form method='post'>
-                <button type='submit'>New</button>
-              </Form>
-            </div>
-            <nav>
-              {contacts.length ? (
-                <ul>
-                  {contacts.map((contact) => (
-                    <li key={contact.id}>
-                      <NavLink
-                        className={({ isActive, isPending }) =>
-                          isActive ? 'active' : isPending ? 'pending' : ''
-                        }
-                        to={`contacts/${contact.id}`}
-                      >
-                        {contact.first || contact.last ? (
-                          <>
-                            {contact.first} {contact.last}
-                          </>
-                        ) : (
-                          <i>No Name</i>
-                        )}{' '}
-                        {contact.favorite ? <span>★</span> : null}
-                      </NavLink>
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <p>
-                  <i>No Contacts</i>
-                </p>
-              )}
-            </nav>
-          </div>
-          <div
-            className={navigation.state === 'loading' && !searching ? 'loading' : ''}
-            id='detail'
+          <Drawer
+            contacts={contacts}
+            q={q}
           >
             {children}
-          </div>
+          </Drawer>
         </ChakraProvider>
         <ScrollRestoration />
         <Scripts />
